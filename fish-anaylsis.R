604faba17637536e785b0eb2e71a4570ee4602e0
Ogonna <Preciousisla235@gmail.com>
2023-12-05 18:21:53 +0000

    Code

---
+++ fish-anaylsis.R
@@ -0,0 +1,7 @@
+library(dplyr)
+
+download.file("https://datacarpentry.org/semester-biology/data/Gaeta_etal_CLC_data.csv", "Gaeta_etal_CLC_data")
+fish_data <- read.csv("Gaeta_etal_CLC_data.csv")
+
+fish_data_cat <- fish_data |>
+  mutate(length_cat <- ifelse(length > 200, "big", "small"))
