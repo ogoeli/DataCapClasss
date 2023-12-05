4f93056745fdf7cf58b7fc7d513db0c482df9d6a
Ogonna <Preciousisla235@gmail.com>
2023-12-05 18:25:23 +0000

    changed size cutoff for new column

--- fish-anaylsis.R
+++ fish-anaylsis.R
@@ -4,4 +4,4 @@ download.file("https://datacarpentry.org/semester-biology/data/Gaeta_etal_CLC_da
 fish_data <- read.csv("Gaeta_etal_CLC_data.csv")
 
 fish_data_cat <- fish_data |>
-  mutate(length_cat <- ifelse(length > 200, "big", "small"))
+  mutate(length_cat <- ifelse(length > 300, "big", "small"))
